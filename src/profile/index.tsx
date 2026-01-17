16b7bc38ebf124f8c59a613c7e48df7d85197e4f
Bryan Maynard <bsgbryan@gmail.com>
2026-01-17 16:44:57 -0600

    Started work on Profile
    Also added some settings for Zed

---
+++ src/profile/index.tsx
@@ -0,0 +1,20 @@
+import './styles.scss'
+
+export default () =>
+	<section id="profile">
+		<img src="https://i.pravatar.cc/250?u=bsgbryan@gmail.com" />
+		<h1>bsgbryan</h1>
+		<h3>Currently Playing</h3>
+		<ol>
+			<li>Unicorn Overlord</li>
+			<li>Cyberpunk 2077</li>
+			<li>DOOM: The Dark Ages</li>
+			<li>Super Mario Wonder</li>
+		</ol>
+		<h3>Plays on</h3>
+		<ul>
+			<li>Steam</li>
+			<li>PS5</li>
+			<li>Switch</li>
+		</ul>
+	</section>
