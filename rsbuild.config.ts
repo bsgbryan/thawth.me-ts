fbb41a9e74efb62439cedf0fe0d29035a4a31f81
Bryan Maynard <bsgbryan@gmail.com>
2026-01-18 16:01:05 -0600

    Move template out of the src dir
    It was confusing RsBuild

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,31 +1,33 @@
-import { defineConfig } from "@rsbuild/core"
-import { pluginReact } from "@rsbuild/plugin-react"
-import { pluginSass } from "@rsbuild/plugin-sass"
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
+import { pluginSass } from "@rsbuild/plugin-sass";
 
 export default defineConfig({
 	output: {
-		distPath: '../thawth.me-rs/serve',
+		distPath: "../thawth.me-rs/serve",
 	},
-  html: {
-    template: './src/template.html',
-    title: 'thawth.me',
-    tags: [{
-      tag: 'script',
-      append: false,
-      attrs: {
-        defer: true,
-        crossorigin: true,
-        src: 'https://cdnjs.cloudflare.com/ajax/libs/jsmediatags/3.9.5/jsmediatags.min.js',
-      },
-    }],
-  },
-  plugins: [
-    pluginReact({
-      swcReactOptions: {
-        runtime: 'automatic',
-        importSource: '@bsgbryan/rnzlr',
-      },
-    }),
-    pluginSass()
-  ],
-})
+	html: {
+		template: "./static/template.html",
+		title: "thawth.me",
+		tags: [
+			{
+				tag: "script",
+				append: false,
+				attrs: {
+					defer: true,
+					crossorigin: true,
+					src: "https://cdnjs.cloudflare.com/ajax/libs/jsmediatags/3.9.5/jsmediatags.min.js",
+				},
+			},
+		],
+	},
+	plugins: [
+		pluginReact({
+			swcReactOptions: {
+				runtime: "automatic",
+				importSource: "@bsgbryan/rnzlr",
+			},
+		}),
+		pluginSass(),
+	],
+});
